8bffe0ff40c94725ed7cf39995b9f176001d2b01
amaya064 <amayabandara0604@gmail.com>
2024-11-21 10:02:05 +0530

    edit sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,17 +1,56 @@
+import { useState } from 'react'
 import {Link} from 'react-router-dom'
 
 export default function SignUp() {
+   const[formData,setFormData] = useState ({})
+   const [error, setError] = useState(null);
+   const [loading, setloading] = useState(false);
+   const handleChange = (e) => {
+      setFormData ({
+        ...formData,
+        [e.target.id]: e. target.value,
+      });
+   };
+
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      setLoading(true);
+    const res = await fetch ('/api/auth/signup', 
+    {
+    
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    if (data.success == false){
+      setError(data.message);
+      setLoading(false);
+      return;
+    }
+    setLoading(false);
+    }catch (error) {
+        setLoading(false);
+    }
+    
+   
+   };
+
+  console.log(formData);
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>
           Sign Up
       </h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' classsName="border p-3 rounded-lg" id="username" />
-        <input type="email" placeholder='email' classsName="border p-3 rounded-lg" id="email" />
-        <input type="password" placeholder='password' classsName="border p-3 rounded-lg" id="password" />
-        <button  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          Sign Up
+      <form onSubmit={handleSubmit}className='flex flex-col gap-4'>
+        <input type="text" placeholder='username' classsName="border p-3 rounded-lg" id="username" onChange={handleChange}/>
+        <input type="email" placeholder='email' classsName="border p-3 rounded-lg" id="email" onChange={handleChange}/>
+        <input type="password" placeholder='password' classsName="border p-3 rounded-lg" id="password" onChange={handleChange}/>
+        <button  disable = {Loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {Loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
 
